9bb6df9a65f3805198acdf57f65c5e1d1c04bedf
Peter <leander.peter@gmail.com>
2024-07-18 10:23:08 +0300

    Fix line drawing algorithm

--- draw_line.c
+++ draw_line.c
@@ -17,41 +17,36 @@
 static int abs(int n)
 {
 	if (n < 0)
-		return (n * -1);
+		return (-n);
 	return (n);
 }
+
 static void	plot_low(t_point start, t_point end, mlx_image_t *img)
 {
 	int	dx;
 	int	dy;
 	int	d;
-	int	x;
-	int	y;
 	int	y_i;
 
 	dx = end.x - start.x;
-	dy = end.y = start.y;
+	dy = end.y - start.y;
 	y_i = 1;
 	if (dy < 0)
 	{
 		y_i = -1;
 		dy = -dy;
 	}
-	x = start.x;
-	y = start.y;
-	d = 2 * dy - dx;
-	
-	while (x <= end.x)
+	d = (2 * dy) - dx;
+	while (start.x <= end.x)
 	{
-		printf("Plotting x: %d, y: %d\n", x, y);
-		mlx_put_pixel(img, x, y, start.color);
+		mlx_put_pixel(img, start.x, start.y, start.color);
 		if (d > 0)
 		{
-			y = y + 1;
-			d = d - 2 * dx;
+			start.y = start.y + y_i;
+			d = d + (2 * (dy - dx));
 		}
 		d = d + 2 * dy;
-		x++;
+		start.x++;
 	}
 }
 
@@ -74,19 +69,18 @@ static void	plot_high(t_point start, t_point end, mlx_image_t *img)
 
 void	draw_line(t_point start, t_point end, mlx_image_t *img)
 {
-	if (abs(end.y - start.y) < abs(end.x - end.y))
+	if (abs(end.y - start.y) < abs(end.x - start.x))
 	{
 		if (start.x > end.x)
-			plot_low(start, end, img);
-		else
 			plot_low(end, start, img);
+		else
+			plot_low(start, end, img);
 	}
 	else
 	{
 		if (start.y > end.y)
-			plot_high(start, end, img);
-		else
 			plot_high(end, start, img);
+		else
+			plot_high(start, end, img);
 	}
 }
-
