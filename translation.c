8e1bfb8f0bfa2558d77001acfaf05deb71f82176
Peter Leander <pleander@student.hive.fi>
2024-08-19 14:46:09 +0300

    Optimize translation calculation

--- translation.c
+++ translation.c
@@ -11,36 +11,13 @@
 /* ************************************************************************** */
 
 #include "fdf.h"
-
+// Find middle point in map, place in the middle of the window
 void	calculate_translation(mlx_t *mlx, t_map *map)
 {
-	int		x[2];
-	int		y[2];
-	size_t	col;
-	size_t	row;
+	t_point2d middle_point;
 
-	x[0] = mlx->width;
-	x[1] = 0;
-	y[0] = mlx->height;
-	y[1] = 0;
-	row = 0;
-	while (row < map->rows)
-	{
-		col = 0;
-		while (col < map->columns)
-		{
-			if (map->sc[row][col].x < x[0])
-				x[0] = map->sc[row][col].x;
-			if (map->sc[row][col].x > x[1])
-				x[1] = map->sc[row][col].x;
-			if (map->sc[row][col].y < y[0])
-				y[0] = map->sc[row][col].y;
-			if (map->sc[row][col].y > y[1])
-				y[1] = map->sc[row][col].y;
-			col++;
-		}
-		row++;
-	}
-	map->settings->x_trans = ((mlx->width - (x[1] - x[0])) / 2) - x[0];
-	map->settings->y_trans = ((mlx->height - (y[1] - y[0])) / 2) - y[0];
+	middle_point = map->sc[map->rows / 2][map->columns / 2];
+	
+	map->settings->x_trans = (mlx->width / 2) - middle_point.x;
+	map->settings->y_trans = (mlx->height / 2) - middle_point.y;
 }
