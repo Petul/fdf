9a553bcd6e368a1e22661347990c6cb11dd4b0db
Peter <leander.peter@gmail.com>
2024-08-27 13:40:44 +0300

    Fix direction of translation

--- translation.c
+++ translation.c
@@ -19,5 +19,5 @@ void	calculate_translation(t_map *map)
 	middle_point = map->sc[map->model->rows / 2][map->model->columns / 2];
 	
 	map->settings->x_trans = (map->img->width / 2) - middle_point.x + map->settings->x_offset;
-	map->settings->y_trans = (map->img->height / 2) - middle_point.y + map->settings->y_offset;
+	map->settings->y_trans = (map->img->height / 2) - middle_point.y - map->settings->y_offset;
 }
